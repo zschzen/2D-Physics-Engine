9c7a5dbe26233ee0240b2892ef4a0488ffee9f7b
Leandro Peres <leandroperes@protonmail.com>
2023-09-12 09:44:54 -0300

    Constraint fixes & Impulses implementation

--- src/Physics/Constraint.cpp
+++ src/Physics/Constraint.cpp
@@ -18,11 +18,11 @@ MatMN Constraint::GetInvMassMatrix() const {
     // a
     result.rows[0][0] = a->inverseMass;
     result.rows[1][1] = a->inverseMass;
-    result.rows[2][2] = a->inverseMass;
+    result.rows[2][2] = a->inverseI;
     // b
     result.rows[3][3] = b->inverseMass;
     result.rows[4][4] = b->inverseMass;
-    result.rows[5][5] = b->inverseMass;
+    result.rows[5][5] = b->inverseI;
     return result;
 }
 
@@ -63,38 +63,48 @@ JointConstraint::JointConstraint(Body *a, Body *b, const Vec2 &anchor) : Constra
 }
 
 void JointConstraint::Solve() {
-    // Load the Jacobian
-    // Get anchor point position in world space
+    // Get the anchor point position in world space
     const Vec2 pa = a->GetWorldPoint(aPoint);
     const Vec2 pb = b->GetWorldPoint(bPoint);
-    
+
     const Vec2 ra = pa - a->position;
     const Vec2 rb = pb - b->position;
-    
-    Vec2 J1 = pa - pb * 2.0f;
-    jacobian.rows[0][0] = J1.x;             // A linear velocity in the x direction
-    jacobian.rows[0][1] = J1.y;             // A linear velocity in the y direction
-    
-    float J2 = ra.Cross(pa - pb) * 2.0f;
-    jacobian.rows[0][2] = J2;               // A angular velocity    
 
-    Vec2 J3 = (pb - pa) * 2.0f;
-    jacobian.rows[0][3] = J3.x;             // B linear velocity in the x direction
-    jacobian.rows[0][4] = J3.y;             // B linear velocity in the y direction
+    jacobian.Zero();
 
-    float J4 = rb.Cross(pb - pa) * 2.0f;
-    jacobian.rows[0][5] = J4;               // B angular velocity
-    
+    Vec2 J1 = (pa - pb) * 2.0;
+    jacobian.rows[0][0] = J1.x; // A linear velocity.x
+    jacobian.rows[0][1] = J1.y; // A linear velocity.y
+
+    float J2 = ra.Cross(pa - pb) * 2.0;
+    jacobian.rows[0][2] = J2;   // A angular velocity
+
+    Vec2 J3 = (pb - pa) * 2.0;
+    jacobian.rows[0][3] = J3.x; // B linear velocity.x
+    jacobian.rows[0][4] = J3.y; // B linear velocity.y
+
+    float J4 = rb.Cross(pb - pa) * 2.0;
+    jacobian.rows[0][5] = J4;   // B angular velocity
+
     const VecN V = GetVelocities();
     const MatMN invM = GetInvMassMatrix();
+
+    const MatMN J = jacobian;
     const MatMN Jt = jacobian.Transpose();
 
-    // Calculate the numerators (Ax = b)
-    MatMN lhs = jacobian * invM * Jt;       // A
-    VecN rhs = -((jacobian * V) + 0.0f);    // B
-    
-    VecN lambda = SolveGaussSeidel(lhs, rhs);
-    
-    // TODO: Compute the lambda -> Impulse to apply to the bodies
-    
+    // Calculate the numerator
+    MatMN lhs = J * invM * Jt; // A
+    VecN rhs = J * V * -1.0f;  // b
+
+    // Solve the values of lambda using Ax=b (Gaus-Seidel method)
+    VecN lambda = MatMN::SolveGaussSeidel(lhs, rhs);
+
+    // Compute the final impulses with direction and magnitude
+    VecN impulses = Jt * lambda;
+
+    // Apply the impulses to both A and B
+    a->ApplyImpulseLinear(Vec2(impulses[0], impulses[1])); // A linear impulse
+    a->ApplyImpulseAngular(impulses[2]);                   // A angular impulse
+    b->ApplyImpulseLinear(Vec2(impulses[3], impulses[4])); // B linear impulse
+    b->ApplyImpulseAngular(impulses[5]);                   // B angular impulse
 }
